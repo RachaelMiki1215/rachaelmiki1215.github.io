843173e6265d6dc8a7bf026b27bc598eb10692e3
RachaelMiki1215 <rachaelbuxton1215@gmail.com>
2022-05-04 15:48:30 -0500

    Added logic for collapsible panes.

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,7 +1,5 @@
 const skillDictDivList = Array.from(document.getElementsByClassName('skillDict'));
 
-//console.log(skillDictDivList);
-
 skillDictDivList.forEach(dict => {
     let dictItems = Array.from(dict.getElementsByTagName('li'));
     //console.log(dictItems);
@@ -10,4 +8,20 @@ skillDictDivList.forEach(dict => {
         //console.log(itemLevel.innerHTML);
         item.style.width = (100 * itemLevel.innerHTML / 5) + "%";
     });
+});
+
+const collapsibleList = Array.from(document.getElementsByClassName("collapseTitle"));
+
+collapsibleList.forEach(col => {
+    col.addEventListener("click", function() {
+        var content = this.nextElementSibling;
+        if (this.classList.contains("collapsed")) {
+            content.style.display = "block";
+            this.classList.remove("collapsed");
+        }
+        else {
+            content.style.display = "none";
+            this.classList.toggle("collapsed");
+        }
+    })
 });
